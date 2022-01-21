d10aa6ad9e9431e0524b8f26e30e504c5b1db5b7
Mike Summers <msummers@newrelic.com>
2022-01-21 06:18:43 -0600

    V1

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# Simple script to automate updating the Action without changing the Release Tag
+#
 git commit -a -m "$1"
 git push -u origin main
 hash=$(git rev-parse HEAD)
